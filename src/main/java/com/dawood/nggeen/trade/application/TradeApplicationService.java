ca222c9b423dc048ef444db112b4d7c8841b562f
sulaimondawood <sulaimondawod@gmail.com>
2026-08-17 13:24:51 +0100

    Modified trade application service exception thrown

--- src/main/java/com/dawood/nggeen/trade/application/TradeApplicationService.java
+++ src/main/java/com/dawood/nggeen/trade/application/TradeApplicationService.java
@@ -5,6 +5,7 @@ import com.dawood.nggeen.shared.exception.NggeenException;
 import com.dawood.nggeen.trade.api.rest.dto.OrderResponse;
 import com.dawood.nggeen.trade.api.rest.dto.PlaceOrderRequest;
 import com.dawood.nggeen.trade.event.DomainEvent;
+import com.dawood.nggeen.trade.exception.InvalidOrderRequestException;
 import com.dawood.nggeen.trade.infrastructure.journal.chronicle.ChronicleQueueService;
 import com.dawood.nggeen.trade.mapper.OrderMapper;
 import com.dawood.nggeen.trade.model.Order;
@@ -16,6 +17,7 @@ import com.dawood.nggeen.trade.service.OrderBookRegistry;
 import com.github.f4b6a3.uuid.UuidCreator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -34,7 +36,7 @@ public class TradeApplicationService {
 
     public void processIncomingOrder(PlaceOrderRequest orderRequest) {
         if (orderRequest == null) {
-            throw new IllegalArgumentException("Invalid Order Request");
+            throw new InvalidOrderRequestException(ErrorCode.BAD_REQUEST, "Order request must not be null", HttpStatus.BAD_REQUEST);
         }
 
         OrderBook instrumentOrderBook = orderBookRegistry.getByInstrumentSymbol(orderRequest.getSymbol());
