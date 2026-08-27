6c9f48d4788d562a9a5c14a31271e9521ae67841
sulaimondawood <sulaimondawod@gmail.com>
2026-08-27 12:16:24 +0100

    finished phase 4

--- src/main/java/com/dawood/nggeen/trade/api/rest/OrderController.java
+++ src/main/java/com/dawood/nggeen/trade/api/rest/OrderController.java
@@ -1,6 +1,7 @@
 package com.dawood.nggeen.trade.api.rest;
 
 import com.dawood.nggeen.shared.dto.ApiResponse;
+import com.dawood.nggeen.trade.api.rest.dto.CancelOrderRequest;
 import com.dawood.nggeen.trade.api.rest.dto.OrderResponse;
 import com.dawood.nggeen.trade.api.rest.dto.PlaceOrderRequest;
 import com.dawood.nggeen.trade.application.TradeApplicationService;
@@ -12,9 +13,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/trades")
+@RequestMapping("/orders")
 @RequiredArgsConstructor
-public class TradeController {
+public class OrderController {
     private final TradeApplicationService tradeService;
 
     @PostMapping
@@ -23,9 +24,15 @@ public class TradeController {
         return ResponseEntity.accepted().body(ApiResponse.successMessage("Your order has been submitted"));
     }
 
-    @GetMapping
+    @GetMapping("/active")
     public ResponseEntity<ApiResponse<List<OrderResponse>>> getActiveOrders() {
-        return ResponseEntity.accepted().body(ApiResponse.success(tradeService.getActiveOrders()));
+        return ResponseEntity.ok(ApiResponse.success(tradeService.getActiveOrders()));
+    }
+
+    @PostMapping("/cancel")
+    public ResponseEntity<ApiResponse<List<OrderResponse>>> cancelOrder(@Valid @RequestBody CancelOrderRequest request) {
+        tradeService.cancelRestingOrder(request);
+        return ResponseEntity.accepted().body(ApiResponse.successMessage("Your request has been submitted"));
     }
 
 }
